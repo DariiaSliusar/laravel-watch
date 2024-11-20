68f58dd281c7cbd9dd732528de7751fbb122fa04
Dariia Sliusar <dariiasliusar@gmail.com>
2024-11-20 11:00:56 +0100

    head

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+    ],
+    theme: {
+        extend: {},
+        container: {
+            center:true,
+        }
+    },
+    plugins: [],
+}
+
