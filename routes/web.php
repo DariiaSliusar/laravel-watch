7157993e0c691597e0792a0203f0a0d5f32f8ba4
Dariia Sliusar <dariiasliusar@gmail.com>
2024-11-20 13:58:52 +0100

    head second commit

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'index');
+Route::view('/', 'index')->name('index');
+Route::view('/courses','courses')->name('courses');
+Route::view('/contact','contact')->name('contact');
+Route::view('/login','login')->name('login');
+Route::view('/register','register')->name('register');
 
